75557e55edeef63c7722160fb7310ff9a3f234c2
Yoshi Tashiro <tashiro@quartile.co>
2019-02-27 13:47:14 +0900

    Add account analytic type (#1)
    * add account_analytic_type
    
    * add category field
    
    * add project_analytic

---
+++ project_analytic/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Analytic",
+    "description": """
+- Facilicates analytic analysis by creating links to analytic lines from 
+project
+    """,
+    "version": "12.0.1.0.0",
+    "category": "Project",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        'account_analytic_type',
+        'project',
+    ],
+    "data": [
+        'views/project_views.xml',
+    ],
+}
