c857d6123c3b0b7ad9cb512bdf8b9d9016720b86
Yoshi Tashiro <tashiro@quartile.co>
2019-02-28 21:06:37 +0800

    prep separate fields per category per type

--- project_analytic/models/project.py
+++ project_analytic/models/project.py
@@ -12,12 +12,82 @@ class Project(models.Model):
         'analytic_project_id',
         domain=[('category', '=', 'plan'),
                 ('analytic_type', '=', 'sales')],
-        string='Analytic Lines (Plan)',
+        string='Sales (Plan)',
     )
     analytic_line_actual_sales_ids = fields.One2many(
         'account.analytic.line',
         'analytic_project_id',
         domain=[('category', '=', 'actual'),
                 ('analytic_type', '=', 'sales')],
-        string='Analytic Lines (Actual)',
+        string='Sales (Actual)',
+    )
+    analytic_line_plan_labour_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'plan'),
+                ('analytic_type', '=', 'labour')],
+        string='Labour (Plan)',
+    )
+    analytic_line_actual_labour_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'actual'),
+                ('analytic_type', '=', 'labour')],
+        string='Labour (Actual)',
+    )
+    analytic_line_plan_pack_mat_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'plan'),
+                ('analytic_type', '=', 'pack_mat')],
+        string='Packaging Materials (Plan)',
+    )
+    analytic_line_actual_pack_mat_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'actual'),
+                ('analytic_type', '=', 'pack_mat')],
+        string='Packaging Materials (Actual)',
+    )
+    analytic_line_plan_outsourcing_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'plan'),
+                ('analytic_type', '=', 'outsourcing')],
+        string='Outsourcing (Plan)',
+    )
+    analytic_line_actual_outsourcing_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'actual'),
+                ('analytic_type', '=', 'outsourcing')],
+        string='Outsourcing (Actual)',
+    )
+    analytic_line_plan_logistics_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'plan'),
+                ('analytic_type', '=', 'logistics')],
+        string='Logistics (Plan)',
+    )
+    analytic_line_actual_logistics_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'actual'),
+                ('analytic_type', '=', 'logistics')],
+        string='Logistics (Actual)',
+    )
+    analytic_line_plan_expenses_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'plan'),
+                ('analytic_type', '=', 'expenses')],
+        string='Expenses (Plan)',
+    )
+    analytic_line_actual_expenses_ids = fields.One2many(
+        'account.analytic.line',
+        'analytic_project_id',
+        domain=[('category', '=', 'actual'),
+                ('analytic_type', '=', 'expenses')],
+        string='Expenses (Actual)',
     )
