f2847f8f8cff667210c54d14655d04d64e962948
Yoshi Tashiro <tashiro@quartile.co>
2019-03-09 16:44:04 +0900

    Upd3 analytic analysis (#4)
    * fix wrong field references
    
    * fix labour code, add sale_id to analytic line
    
    * add team_id, sale_user_id, employee_id, department_id
    
    * update translation

--- project_analytic/models/account_analytic_line.py
+++ project_analytic/models/account_analytic_line.py
@@ -16,6 +16,38 @@ class AccountAnalyticLine(models.Model):
         inverse='_set_account_id',
         store=True,
     )
+    sale_id = fields.Many2one(
+        'sale.order',
+        string='Sales Order',
+        compute='_compute_analytic_project_id',
+        store=True,
+        readonly=True,
+    )
+    team_id = fields.Many2one(
+        related='sale_id.team_id',
+        store=True,
+        readonly=True,
+    )
+    sale_user_id = fields.Many2one(
+        related='sale_id.user_id',
+        store=True,
+        readonly=True,
+    )
+    # this overrides the standard employee_id field to add compute method
+    employee_id = fields.Many2one(
+        # 'hr.employee',
+        # string='Employee',
+        compute='_compute_employee_id',
+        store=True,
+        readonly=True,
+    )
+    department_id = fields.Many2one(
+        'hr.department',
+        string='Department',
+        compute='_compute_employee_id',
+        store=True,
+        readonly=True,
+    )
 
 
     @api.multi
@@ -24,6 +56,23 @@ class AccountAnalyticLine(models.Model):
         for ln in self:
             projects = ln.account_id and ln.account_id.project_ids
             ln.analytic_project_id = projects and projects[0].id or False
+            # use sudo() for search here - not all users have access to all
+            # the sales orders
+            sale_orders = self.env['sale.order'].sudo().search(
+                [('analytic_account_id', '=', ln.account_id.id),
+                 ('state', '!=', 'cancel')])
+            if len(sale_orders) == 1:
+                ln.sale_id = sale_orders[0].id
+
+    @api.multi
+    @api.depends('user_id')
+    def _compute_employee_id(self):
+        for ln in self:
+            emp_ids = self.env['hr.employee'].search(
+                [('user_id', '=', ln.user_id.id)])
+            if emp_ids:
+                ln.employee_id = emp_ids[0]
+                ln.department_id = emp_ids[0].department_id
 
     @api.multi
     def _set_account_id(self):
@@ -38,5 +87,5 @@ class AccountAnalyticLine(models.Model):
             vals['category'] = 'actual'
             # assumption: project_id is included only for timesheet entries
             if vals.get('project_id'):
-                vals['analytic_type'] = 'labour'
+                vals['analytic_type'] = '20_labour'
         return super(AccountAnalyticLine, self).create(vals)
